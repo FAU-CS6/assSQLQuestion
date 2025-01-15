eb67e18afc3d3847cdedaafe097caaf47392475e
Marco Angerer <marco.angerer@fau.de>
2025-01-15 19:07:56 +0100

    Minor: Remove unneeded daclaration

--- classes/class.assSQLQuestionGUI.php
+++ classes/class.assSQLQuestionGUI.php
@@ -425,10 +425,7 @@ class assSQLQuestionGUI extends assQuestionGUI
      * @access private
      */
     private function addSpecificQuestionFormProperties(\ilPropertyFormGUI $form)
-    {
-        global $lng;
-
-        // Insert the different GUIAreas
+    {        // Insert the different GUIAreas
         $guiAreas = array();
         array_push($guiAreas, new QuestionArea($this->plugin, $this->object));
         array_push($guiAreas, new SequenceArea($this->plugin, $this->object));
