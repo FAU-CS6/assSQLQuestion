38938656d64d24a5f5a49cbb26ae4de405b99fd2
Marco Angerer <marco.angerer@fau.de>
2025-02-18 21:47:55 +0100

    Fix #26 by un-overriding setQuestionTabs()

--- classes/class.assSQLQuestionGUI.php
+++ classes/class.assSQLQuestionGUI.php
@@ -308,86 +308,8 @@ class assSQLQuestionGUI extends assQuestionGUI
         $output = '';
         return $this->object->prepareTextareaOutput($output, true);
     }
-
-
-    /**
-     * Sets the ILIAS tabs for this question type
-     * called from ilObjTestGUI and ilObjQuestionPoolGUI
-     */
-    public function setQuestionTabs(): void
-    {
-        global $DIC;
-        $rbacsystem = $DIC->rbac()->system();
-        $ilTabs = $DIC->tabs();
-
-        $this->ctrl->setParameterByClass("ilpageobjectgui", "q_id", $_GET["q_id"]);
-        include_once "./Modules/TestQuestionPool/classes/class.assQuestion.php";
-        $q_type = $this->object->getQuestionType();
-
-        if (strlen($q_type)) {
-            $classname = $q_type . "GUI";
-            $this->ctrl->setParameterByClass(strtolower($classname), "sel_question_types", $q_type);
-            $this->ctrl->setParameterByClass(strtolower($classname), "q_id", $_GET["q_id"]);
-        }
-
-        if ($_GET["q_id"]) {
-            if ($rbacsystem->checkAccess('write', $_GET["ref_id"])) {
-                // edit page
-                $ilTabs->addTarget(
-                    "edit_page",
-                    $this->ctrl->getLinkTargetByClass("ilAssQuestionPageGUI", "edit"),
-                    array("edit", "insert", "exec_pg")
-                );
-            }
-
-            $this->addTab_Question($ilTabs);
-        }
-
-        $force_active = false;
-        if ($rbacsystem->checkAccess('write', $_GET["ref_id"])) {
-            $url = "";
-
-            if ($classname) {
-                $url = $this->ctrl->getLinkTargetByClass($classname, "editQuestion");
-            }
-            $commands = $_POST["cmd"];
-
-            // edit question properties
-            $ilTabs->addTarget(
-                "edit_properties",
-                $url,
-                array("editQuestion", "save", "cancel", "saveEdit", "originalSyncForm"),
-                $classname,
-                "",
-                $force_active
-            );
-        }
-
-        // add tab for question feedback within common class assQuestionGUI
-        $this->addTab_QuestionFeedback($ilTabs);
-
-        // add tab for question hint within common class assQuestionGUI
-        $this->addTab_QuestionHints($ilTabs);
-
-        // add tab for question's suggested solution within common class assQuestionGUI
-        $this->addTab_SuggestedSolution($ilTabs, $classname);
-
-
-        // Assessment of questions sub menu entry
-        if ($_GET["q_id"]) {
-            $ilTabs->addTarget(
-                "statistics",
-                $this->ctrl->getLinkTargetByClass($classname, "assessment"),
-                array("assessment"),
-                $classname,
-                ""
-            );
-        }
-
-        $this->addBackTab($ilTabs);
-    }
-
-
+    
+    
     /**
      * Custom member functions only needed in an assSQLQuestionGUI
      */
