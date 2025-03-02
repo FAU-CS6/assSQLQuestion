e375048b192672eed0ac4b1fdf72244104a424a6
Marco Angerer <marco.angerer@fau.de>
2025-03-02 12:33:06 +0100

    Add technical provisions to add the syntax help text later

---
+++ classes/internal/GUI/GUIElements/SequenceArea/class.SyntaxHelp.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Represents the syntax help GUIElement
+ *
+ * @author Marco Angerer <marco.angerer@fau.de>
+ */
+class SyntaxHelp extends GUIElement
+{
+    /**
+     * Returns the html output of the GUI element tailored for the edit page
+     *
+     * @return string The html code of the GUI element
+     * @access public
+     */
+    public function getEditOutput()
+    {
+        $tpl = $this->plugin->getTemplate('Mixed/tpl.il_as_qpl_qpisql_m_syntax_help.html');
+        $tpl->setVariable("TITLE", $this->plugin->txt('ai_sea_eo_syntax_help_title'));
+        $tpl->setVariable("TEXT", $this->plugin->txt('ai_sea_eo_syntax_help_text'));
+        return $tpl->get();
+    }
+
+    /**
+     * Returns the html output of the GUI element tailored for the question output page
+     *
+     * @param ParticipantInput $participant_input A ParticipantInput object containing the existing data
+     * @return string The html code of the GUI element
+     * @access public
+     */
+    public function getQuestionOutput($participant_input)
+    {
+        return "";
+    }
+
+    /**
+     * Returns the html output of the GUI element tailored for the solution output page
+     *
+     * @param ParticipantInput|null $participant_input A ParticipantInput object containing the participant inputs
+     * @return string The html code of the GUI element
+     * @access public
+     */
+    public function getSolutionOutput($participant_input)
+    {
+        return "";
+    }
+
+    /**
+     * Writes the POST data of the edit page into the $object
+     *
+     * @access public
+     */
+    public function writePostData()
+    {
+        // Do nothing
+    }
+
+    /**
+     * Writes the POST data of a participants input into a ParticipantInput object
+     *
+     * @param ParticipantInput $participant_input The ParticipantInput object the POST data is written to
+     * @access public
+     */
+    public function writeParticipantInput($participant_input)
+    {
+        // Do nothing
+    }
+}
