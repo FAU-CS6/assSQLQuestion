80357d420e3a60215f52f202c9d7ac659b0de48c
Marco Angerer <marco.angerer@fau.de>
2025-03-05 14:33:36 +0100

    Remove strict_types in some places until we've got DB type security

--- classes/class.assSQLQuestion.php
+++ classes/class.assSQLQuestion.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Main defintion of the SQLQuestion plugin
  *
--- classes/class.assSQLQuestionGUI.php
+++ classes/class.assSQLQuestionGUI.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * GUI class of the SQLQuestion plugin
  *
